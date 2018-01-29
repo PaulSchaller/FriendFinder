7c77a5d3eb9482b8100d69bd25d7243627474d5b
unknown <paul_schaller@att.net>
2018-01-29 08:50:10 -0500

    adding significant updates to program

---
+++ app/data/friends.js
@@ -0,0 +1,139 @@
+[
+
+{
+	"name": "Kevin Costner",
+	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzNjkwOTE0NV5BMl5BanBnXkFtZTgwMTM0NDI2MDI@._V1_UY317_CR88,0,214,317_AL_.jpg",
+	"scores": [
+				"5",
+				"1",
+				"4",
+				"4",
+				"5",
+				"1",
+				"2",
+				"5",
+				"4",
+				"1"
+			  ]
+},
+
+{
+	"name": "Heather Locklear",
+	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNDI1Mjc5OF5BMl5BanBnXkFtZTcwOTg3OTc1Mg@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+	"scores": [
+				"4",
+				"2",
+				"5",
+				"1",
+				"3",
+				"2",
+				"2",
+				"1",
+				"3",
+				"2"
+			  ]
+},
+
+{
+	"name": "Iman Abdulmajid",
+	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczNzUxNDMwOF5BMl5BanBnXkFtZTYwNzY3ODI4._V1_UY317_CR16,0,214,317_AL_.jpg",
+	"scores": [
+				"5",
+				"2",
+				"2",
+				"2",
+				"4",
+				"1",
+				"3",
+				"2",
+				"5",
+				"5"
+			  ]
+},
+
+{
+	"name": "Mona Grudt",
+	"photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlVZiHhct9GcXhb5DXY0GX9psm671WFzrmGRS8HCNESfaADXzw0g",
+	"scores": [
+				"3",
+				"3",
+				"4",
+				"2",
+				"2",
+				"1",
+				"3",
+				"2",
+				"2",
+				"3"
+			  ]
+},
+
+{
+	"name": "Emilio Estevez",
+	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BOTY2MDM3MDc1OV5BMl5BanBnXkFtZTcwMTQxOTkyOA@@._V1._SX2000_CR15,79,609,709_UY317_CR29,0,214,317_AL_.jpg",
+	"scores": [
+				"4",
+				"3",
+				"4",
+				"1",
+				"5",
+				"2",
+				"5",
+				"3",
+				"1",
+				"4"
+			  ]
+},
+
+{
+	"name": "George Clooney",
+	"photo": "http://www.imdb.com/name/nm0000123/mediaviewer/rm3196301568",
+	"scores": [
+				"4",
+				"4",
+				"2",
+				"3",
+				"2",
+				"2",
+				"3",
+				"2",
+				"4",
+				"5"
+			  ]
+},
+
+{
+	"name": "Michael Tucker",
+	"photo": "http://www.imdb.com/name/nm0875951/mediaviewer/rm2016972288",
+	"scores": [
+				"1",
+				"1",
+				"1",
+				"1",
+				"3",
+				"1",
+				"1",
+				"1",
+				"1",
+				"1"
+			  ]
+}
+
+]
+
+// ===============================================================================
+// DATA
+// Below data will hold all of the waitlist tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+var waitingArray = [
+  {
+    customerName: "Saima",
+    customerEmail: "saima@example.com",
+    phoneNumber: "000-000-0000",
+    customerID: "saimacool"
+  }
+];
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = waitingArray;
