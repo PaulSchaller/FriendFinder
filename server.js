3453e3c5163c1e72ff011b130558daec910b3863
unknown <paul_schaller@att.net>
2018-01-31 16:51:46 -0500

    adding changes to my project

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(bodyParser.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
