0a9e15c78bcd1044da670f25663e5d1034ed3b29
unknown <paul_schaller@att.net>
2018-02-01 00:13:24 -0500

    small updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var friends = [
 {
 	"name": "Kevin Costner",
 	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzNjkwOTE0NV5BMl5BanBnXkFtZTgwMTM0NDI2MDI@._V1_UY317_CR88,0,214,317_AL_.jpg",
-	"scores": [
+	"scores []": [
 				"5",
 				"1",
 				"4",
@@ -20,7 +20,7 @@ var friends = [
 {
 	"name": "Heather Locklear",
 	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNDI1Mjc5OF5BMl5BanBnXkFtZTcwOTg3OTc1Mg@@._V1_UX214_CR0,0,214,317_AL_.jpg",
-	"scores": [
+	"scores []": [
 				"4",
 				"2",
 				"5",
@@ -37,7 +37,7 @@ var friends = [
 {
 	"name": "Iman Abdulmajid",
 	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczNzUxNDMwOF5BMl5BanBnXkFtZTYwNzY3ODI4._V1_UY317_CR16,0,214,317_AL_.jpg",
-	"scores": [
+	"scores []" : [
 				"5",
 				"2",
 				"2",
@@ -54,7 +54,7 @@ var friends = [
 {
 	"name": "Mona Grudt",
 	"photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlVZiHhct9GcXhb5DXY0GX9psm671WFzrmGRS8HCNESfaADXzw0g",
-	"scores": [
+	"scores []": [
 				"3",
 				"3",
 				"4",
@@ -71,7 +71,7 @@ var friends = [
 {
 	"name": "Emilio Estevez",
 	"photo": "https://images-na.ssl-images-amazon.com/images/M/MV5BOTY2MDM3MDc1OV5BMl5BanBnXkFtZTcwMTQxOTkyOA@@._V1._SX2000_CR15,79,609,709_UY317_CR29,0,214,317_AL_.jpg",
-	"scores": [
+	"scores []": [
 				"4",
 				"3",
 				"4",
@@ -88,7 +88,7 @@ var friends = [
 {
 	"name": "George Clooney",
 	"photo": "http://www.imdb.com/name/nm0000123/mediaviewer/rm3196301568",
-	"scores": [
+	"scores []": [
 				"4",
 				"4",
 				"2",
@@ -105,7 +105,7 @@ var friends = [
 {
 	"name": "Michael Tucker",
 	"photo": "http://www.imdb.com/name/nm0875951/mediaviewer/rm2016972288",
-	"scores": [
+	"scores []": [
 				"1",
 				"1",
 				"1",
